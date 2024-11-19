d40ea47820beaebcd272ffd753bd2a82c8fd2ab1
Mubarak <dewriter.sam@gmail.com>
2024-11-19 08:04:38 +0100

    Create a series

--- index.py
+++ index.py
@@ -3,5 +3,10 @@ print("We are learning about Git and GitHub")
 
 print("Let's edit this file")
 print("This is amazing")
+import pandas as pd
+import numpy as np
 
-import this
+
+# Create a Pandas series from a list
+data = ['Mubarak', 'Adeola', 'John']
+s = pd.Series(data)
